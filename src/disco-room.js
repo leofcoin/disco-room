30a637fd7a574409f76ac8cbc25a5c8c60aa79c8
vandeurenglenn <vandeurenglenn@gmail.com>
2019-07-14 20:16:47 +0200

    :metal: ensure we are subcribed before broadcasting
    - connect using '/p2p-circuit/ipfs/'
    - rebroadcast
    - cleanup

--- src/disco-room.js
+++ src/disco-room.js
@@ -24,51 +24,14 @@ export default class DiscoRoom extends PeerMonitor {
     this.ipfs = ipfs;
 
     this.topic = topic;
-    this.peers = [];    
-    
-    ipfs.pubsub.subscribe(topic, async (message) => {
-      console.log(message.data);
-      try {
-        message.data = ab2str(message.data);
-        message.data = JSON.parse(message.data);
-        
-        const { peer, peers } = message.data;
-        if (peer && peer !== this.id && this.peers.indexOf(peer) === -1) {        
-          this.broadcast({ type: 'peerlist', for: peer, peers: this.peers });
-          try {              
-            await this.ipfs.swarm.connect('/ipfs/' + peer);
-            this.peers.push(peer);
-          } catch (e) {
-            console.error(e);  
-          }
-        }
-        else if (message.data.for === this.id && peers && peers.length > 1) {
-          peers.forEach(async peer => {
-            try {              
-              if (this.peers.indexOf(peer) === -1 && peer !== this.id) {
-                await this.ipfs.swarm.connect('/ipfs/' + peer)
-                this.peers.push(peer);
-              }              
-            } catch (e) {
-              console.error(e);  
-            }
-          });
-        }
-      } catch (e) {
-        console.error(e);
-      }      
-      // super.emit('message', message);
-    }, (err, res) => {});
+    this.peers = [];
     
     this._peerJoined = this._peerJoined.bind(this);
     this._peerLeft = this._peerLeft.bind(this);
-    this._subscribed = this._subscribed.bind(this);
+    this._subscribed = this._subscribed.bind(this);    
+    this._onTopicMessage = this._onTopicMessage.bind(this);
     
-    this.init();
-    // this.ipfs.id().then(({ id }) => {
-    // this.broadcast(JSON.stringify({type: 'joining', from: id}))  
-    // })
-    
+    this.init();    
   }
   
   async init() {
@@ -80,50 +43,75 @@ export default class DiscoRoom extends PeerMonitor {
     this.on('error', error => console.error(error));
     this.on('subscribed', this._subscribed);
     
+    await ipfs.pubsub.subscribe(this.topic, this._onTopicMessage);
     this.broadcast({ type: 'peer-joined', peer: this.id });   
   }
 
 
   async broadcast(data) {
-    await this.ipfs.pubsub.publish(this.topic, Buffer.from(str2ab(JSON.stringify(data))));
+    const arrayBuffer = str2ab(JSON.stringify(data));
+    await this.ipfs.pubsub.publish(this.topic, Buffer.from(arrayBuffer), 'ArrayBuffer');
+  }
+  
+  async _onTopicMessage(message) {
+    try {
+      let data = ab2str(message.data);
+      data = JSON.parse(data);      
+      const { peer, peers, from } = data;
+      
+      if (peer && peer !== this.id && this.peers.indexOf(peer) === -1) {        
+        this.broadcast({ type: 'peerlist', for: peer, peers: this.peers });
+        this.peers.push(peer);
+        try {
+          await this.ipfs.swarm.connect('/p2p-circuit/ipfs/' + peer);
+        } catch (e) {
+          console.error(e);  
+        }
+      }
+      else if (message.data.for === this.id && peers && peers.length > 1) {
+        peers.forEach(async peer => {
+          try {              
+            if (this.peers.indexOf(peer) === -1 && peer !== this.id) {
+              this.peers.push(peer);
+              await this.ipfs.swarm.connect('/p2p-circuit/ipfs/' + peer)
+            }              
+          } catch (e) {
+            console.error(e);  
+          }
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   _subscribed() {
     this.subscribed = true;
   }
+  
+  /**
+   * rebroadcast 'peer-joined' message to client connected peers
+   * typically new connected nodes will only have one connection at first.
+   * 
+   * @param {string} peer - base58 string of the peer id
+   * @param {string} type 'peer-joined' - event name
+   * @param {string} from [this.id]
+   */
+  _rebroadcast(peer, type = 'peer-joined') {
+    this.broadcast({ type, peer, from: this.id });
+  }
 
   _peerJoined(peer) {
-    console.log(peer); 
-    if (this.peers.indexOf(peer) === -1) this.peers.push(peer);
-    
-    // this.whisper(peer)
+    console.log(`${peer} joined`);
+    if (this.peers.indexOf(peer) === -1) {
+      this._rebroadcast(peer);
+      this.peers.push(peer);      
+    }
   }
 
   _peerLeft(peer) {
-    this.peers.splice(this.peers.indexOf(peer), 1);
+    console.log(`${peer} left`);
+    const index = this.peers.indexOf(peer);
+    if (index !== -1) this.peers.splice(index, 1);
   }
-
-  // async whisper(peerID, event) {  
-  //   console.log(peerID, 'whisper');  
-  //   event.from = this.id;
-  //   peerID = `/ipfs/${peerID}`;
-  //   const channel = await Channel.open(this.ipfs, peerID);
-  //   // await channel.connect();
-  //   console.log('conne');
-  //   channel.on('message', async (message) => {
-  //     console.log(message);
-  //     if (message.from !== this.id) {
-  //       if (message.type === 'join') {
-  //         const index = message.data.indexOf(this.id);
-  //         if (index !== -1) message.data.splice(index, 1);
-  //         this.ipfs.swarm.connect(message.data);
-  //         channel.close();
-  //       } else {
-  //         await this.whisper(message.from, { type: 'join', from: this.id, data: this.peers});
-  //         channel.close();
-  //       }
-  //     }
-  //   });
-  //   return channel.emit('message', event)
-  // }
 }
